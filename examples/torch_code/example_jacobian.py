a1f52f9ad1f6e484d01cf194e72a7800b6d13530
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2026-05-04 21:39:27 +0530

    minor changes: updated physika example file

--- examples/torch_code/example_jacobian.py
+++ examples/torch_code/example_jacobian.py
@@ -13,15 +13,15 @@ def f_vec(state):
     fy = (x + (y ** 2))
     return torch.stack([torch.as_tensor(fx).float(), torch.as_tensor(fy).float()])
 
-def lotka_volterra(state, theta):
+def lotka_volterra(state, θ):
     x = state[int(0)]
     y = state[int(1)]
-    alpha = theta[int(0)]
-    beta = theta[int(1)]
-    gamma = theta[int(2)]
-    delta = theta[int(3)]
-    dx = ((alpha * x) - ((beta * x) * y))
-    dy = (((delta * x) * y) - (gamma * y))
+    α = θ[int(0)]
+    β = θ[int(1)]
+    γ = θ[int(2)]
+    δ = θ[int(3)]
+    dx = ((α * x) - ((β * x) * y))
+    dy = (((δ * x) * y) - (γ * y))
     return torch.stack([torch.as_tensor(dx).float(), torch.as_tensor(dy).float()])
 
 # === Program ===
@@ -29,8 +29,8 @@ state = torch.as_tensor(torch.tensor([1.0, 2.0])).float().requires_grad_(True)
 J = compute_grad(f_vec, state)
 physika_print(J)
 state = torch.as_tensor(torch.tensor([10.0, 1.0])).float().requires_grad_(True)
-theta = torch.tensor([1.5, 1.0, 3.0, 1.0])
-J_state = compute_grad(lotka_volterra, state, theta, 0)
+θ = torch.tensor([1.5, 1.0, 3.0, 1.0])
+J_state = compute_grad(lotka_volterra, state, θ, 0)
 physika_print(J_state)
-J_theta = compute_grad(lotka_volterra, state, theta, 1)
+J_theta = compute_grad(lotka_volterra, state, θ, 1)
 physika_print(J_theta)
